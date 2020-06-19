49fe73a3ef83ad36075e6f6d581554accf01d77d
linuxs <linuxs@airplug.com>
2020-06-19 15:30:46 +0900

    메시지 변경

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World novak!" << std::endl;
+    std::cout << "Hello, World novak!" << std::endl;
     return 0;
 }
