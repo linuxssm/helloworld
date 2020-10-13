cd5c807962b86b5f4a56f6ae826f06273ab9cae5
linuxs <linuxs@airplug.com>
2020-10-13 10:02:55 +0900

    gggg

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 int main() {
     std::cout << "Hello, World novak!" << std::endl;
     std::cout << "Hello, World novak!" << std::endl;
+    std::cout << "Hello, World novak!" << std::endl;
+    std::cout << "Hello, World novak!" << std::endl;
+
     return 0;
 }
